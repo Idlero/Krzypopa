ab481f8d2d3ef6c32de80bb0ef98d8a55ff2074f
Idlero <sebw20@gmail.com>
2013-12-21 22:05:41 +0100

    Parser_FormSubmit

--- war/src/main/java/pl/bookingsystem/webapp/admin/action/SubmitDataAction.java
+++ war/src/main/java/pl/bookingsystem/webapp/admin/action/SubmitDataAction.java
@@ -5,6 +5,9 @@ import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import pl.bookingsystem.db.entity.Client;
 
 import java.io.InputStream;
@@ -24,7 +27,7 @@ public class SubmitDataAction extends ActionSupport {
         return message;
     }
 
-
+    private Client client;
     private String data;
 
     @Action(value = "reservation-add", results = {
@@ -34,16 +37,34 @@ public class SubmitDataAction extends ActionSupport {
     public String reservationAdd() {
         try {
 
+            JSONObject obj = new JSONObject();
+            JSONParser parser = new JSONParser();
+
+            try {
+                String s = jasonConvert(data);
+                Object object = parser.parse(s);
+                JSONObject jsonObject = (JSONObject) object;
+                String name = (String) jsonObject.get("first_name");
+
+                System.out.println(name);
+            } catch (ParseException pe) {
+                System.out.println("position: " + pe.getPosition());
+                System.out.println(pe);
+
+
+            }
+
+
             System.out.println(data);
 
-// ClientDAO clientManager = new ClientDAOImpl();
+            // ClientDAO clientManager = new ClientDAOImpl();
             // clientManager.save(data);
-           // message = new ByteArrayInputStream("Data has been saved".getBytes());
+            // message = new ByteArrayInputStream("Data has been saved".getBytes());
             return SUCCESS;
 
         } catch (Exception e) {
 
-          //  message = new ByteArrayInputStream("Data hasn't been saved".getBytes());
+            //  message = new ByteArrayInputStream("Data hasn't been saved".getBytes());
             return ERROR;
         }
 
@@ -56,4 +77,17 @@ public class SubmitDataAction extends ActionSupport {
     public void setData(String data) {
         this.data = data;
     }
+
+    public String jasonConvert(String serialize) {
+        String conv = "{\"" + serialize + "\"}";
+        conv = conv.replaceAll("=", "\":\"");
+        conv = conv.replaceAll("&", "\",\"");
+        System.out.println(conv);
+        return conv;
+    }
+
+
 }
+
+
+
